bba311ab7038e2999bafc787569dd40306198dcb
David Keeney <dkeeney@travelbyroad.net>
2024-07-29 14:13:57 +0800

    Removed prior FiveDown version

--- FiveDown/objects.js
+++
@@ -1,8 +0,0 @@
-// var parser = new formulaParser.Parser();
-// parser.setVariable("h", 4);
-
-// console.log(parser.parse("h^2")); // It returns `Object {error: null, result: 14}`
-
-//! TO DO:
-//? 6. Error Checking and Validation
-//? 4. Provide good docs
