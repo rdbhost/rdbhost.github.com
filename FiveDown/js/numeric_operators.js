7cf307cc65b5b53a05197ccdfdc01568d2f26a38
David Keeney <dkeeney@travelbyroad.net>
2024-10-08 14:04:57 +0800

    begin making ops robust

--- FiveDown/js/numeric_operators.js
+++ FiveDown/js/numeric_operators.js
@@ -1,39 +1,62 @@
 
+function getType(a) {
+
+  if ([true, false].indexOf(a) > -1)
+    return 'boolean'
+
+  if (Array.isArray(a)) {
+
+    if (a.length < 2 || a.length > 3)
+      throw new Error(`bad vector length ${a.length}`)
+
+    let t = a.filter((x) => x+0 === x)
+    if (t.length === a.length)
+      return 'vector'
+
+    throw new Error(`bad data ${typeof a}`)
+  }
+
+  if (a+0 === a)
+    return 'number'
+
+  throw new Error('bad input ${a}')
+}
+
 var unaryOps = {
 
-    sin: Math.sin,
-    cos: Math.cos,
-    tan: Math.tan,
-    asin: Math.asin,
-    acos: Math.acos,
-    atan: Math.atan,
-    sinh: Math.sinh,
-    cosh: Math.cosh,
-    tanh: Math.tanh,
-    asinh: Math.asinh,
-    acosh: Math.acosh,
-    atanh: Math.atanh,
-    sqrt: Math.sqrt,
-    cbrt: Math.cbrt,
-    log: Math.log,
-    log2: Math.log2,
-    ln: Math.log,
-    lg: Math.log10,
-    log10: Math.log10,
-    expm1: Math.expm1,
-    log1p: Math.log1p,
-    abs: Math.abs,
-    ceil: Math.ceil,
-    floor: Math.floor,
-    round: Math.round,
-    trunc: Math.trunc,
-    '-': (x) => -x,
-    '+': Number,
-    exp: Math.exp,
+    sin: function(a) { return Math.sin(a) },
+    cos: function(a) { return Math.cos(a) },
+    tan: function(a) { return Math.tan(a) },
+    asin: function(a) { return Math.asin(a) },
+    acos: function(a) { return Math.acos(a) },
+    atan: function(a) { return Math.atan(a) },
+    sinh: function(a) { return Math.sinh(a) },
+    cosh: function(a) { return Math.cosh(a) },
+    tanh: function(a) { return Math.tanh(a) },
+    asinh: function(a) { return Math.asinh(a) },
+    acosh: function(a) { return Math.acosh(a) },
+    atanh: function(a) { return Math.atanh(a) },
+    sqrt: function(a) { return Math.sqrt(a) },
+    cbrt: function(a) { return Math.cbrt(a) },
+    log: function(a) { return Math.log(a) },
+    log2: function(a) { return Math.log2(a) },
+    ln: function(a) { return Math.log(a) },
+    lg: function(a) { return Math.log10(a) },
+    log10: function(a) { return Math.log10(a) },
+    expm1: function(a) { return Math.expm1(a) },
+    log1p: function(a) { return Math.log1p(a) },
+    abs: function(a) { return Math.abs(a) },
+    ceil: function(a) { return Math.ceil(a) },
+    floor: function(a) { return Math.floor(a) },
+    round: function(a) { return Math.round(a) },
+    trunc: function(a) { return Math.trunc(a) },
+    '-': function(a) { return -1 * a },
+    '+': function(a) { return 1 * a },
+    exp: function(a) { return Math.exp(a) },
     not: (x) => !x,
-    length: (x) => stringOrArrayLength(x),
-    '!': (x) => factorial(x),
-    sign: Math.sign
+    length: function(x) { return stringOrArrayLength(x) },
+    '!': function(x) { return factorial(x) },
+    sign: function(x) { return Math.sign(x) }
 }
 
 var binaryOps = {
@@ -65,13 +88,13 @@ var ternaryOps = {
 
 var functions = {
   
-    random: Math.random,
+    random: function(a) { return Math.random(a) },
     fac: factorial,
     min: min,
     max: max,
-    hypot: Math.hypot,
+    hypot: function(a) { return Math.hypot(a) },
     pow: (a,b) => power(a,b),
-    atan2: Math.atan2,
+    atan2: function(a) { return Math.atan2(a) },
 //    'if': condition,
 //    gamma: gamma,
     roundTo: roundTo,
@@ -83,6 +106,7 @@ var functions = {
 }
 
 function power(a,b) {
+
   let r = Math.pow(a,b)
   if (Number.isNaN(r))
     throw new Error(`bad arguments to ^ ${a} ${b}`)
@@ -90,10 +114,12 @@ function power(a,b) {
 }
 
 function arrayIndex(array, index) {
+
   return array[index | 0];
 }
 
 function contains(array, obj) {
+
   for (var i = 0; i < array.length; i++) {
     if (array[i] === obj) {
       return true;
@@ -103,10 +129,12 @@ function contains(array, obj) {
 }
 
 function factorial(a) { // a!
+
     return gamma(a + 1);
 }
   
 function stringOrArrayLength(s) {
+
     if (Array.isArray(s)) {
       return s.length;
     }
@@ -114,6 +142,7 @@ function stringOrArrayLength(s) {
 }
 
 function roundTo(value, exp) {
+
     // If the exp is undefined or zero...
     if (typeof exp === 'undefined' || +exp === 0) {
       return Math.round(value);
@@ -142,12 +171,14 @@ function arrayJoin(sep, a) {
 }
   
 function inOperator(a, b) {
+
   if (!Array.isArray(b))
     throw new Error(`'in' operator needs a vector, not ${typeof b}`)
   return contains(b, a);
 }
 
 function max(array) {
+
     if (arguments.length === 1 && Array.isArray(array)) {
       return Math.max.apply(Math, array);
     } else {
@@ -156,6 +187,7 @@ function max(array) {
   }
   
 function min(array) {
+
     if (arguments.length === 1 && Array.isArray(array)) {
       return Math.min.apply(Math, array);
     } else {
